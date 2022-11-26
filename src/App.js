cfddf946bd79b0b046deeed9a54b55ba2a82bed1
Utkarsh <Utkarshsinghclash@gmail.com>
2022-11-26 11:20:33 +0530

    inital repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// base url of API
+const BASE_URL = "http://localhost:4000/";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null, // to store selected file
+      handleResponse: null, // handle the API response
+      imageUrl: null // to store uploaded image path
+    };
+  }
+
+  // handle change event of input file
+  onChangeFile = event => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+
+  // handle click event of the upload button
+  handleUpload = () => {
+    const { selectedFile } = this.state;
+    if (!selectedFile) {
+      this.setState({
+        handleResponse: {
+          isSuccess: false,
+          message: "Please select image to upload."
+        }
+      });
+      return false;
+    }
+    const formData = new FormData();
+    formData.append("dataFile", selectedFile, selectedFile.name);
+    axios
+      .post(BASE_URL + "uploadfile", formData)
+      .then(response => {
+        this.setState({
+          handleResponse: {
+            isSuccess: response.status === 200,
+            message: response.data.message
+          },
+          imageUrl: BASE_URL + response.data.file.path
+        });
+      })
+      .catch(err => {
+        alert(err.message);
+      });
+  };
+
+  render() {
+    const { handleResponse, imageUrl } = this.state;
+    return (
+      <>
+        <div className="App">
+          <h4>Image Upload in ReactJS - Clue Mediator</h4>
+
+          <p className="title">Select Image:</p>
+          <div style={{ marginBottom: 10 }}>
+            <input type="file" onChange={this.onChangeFile} />
+          </div>
+          <input type="button" value="Upload" onClick={this.handleUpload} />
+          {handleResponse && (
+            <p className={handleResponse.isSuccess ? "success" : "error"}>
+              {handleResponse.message}
+            </p>
+          )}
+
+          <p className="title" style={{ marginTop: 30 }}>
+            Uploaded Image:
+          </p>
+          {imageUrl && (
+            <img src={imageUrl} alt="Uploaded File" height="100" width="100" />
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
